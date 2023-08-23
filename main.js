59a1299fb727e8825110be75b54d3e0227d5f20d
KimHS17 <skes01149@naver.com>
2023-08-23 19:46:31 +0900

    Express_author

--- main.js
+++ main.js
@@ -32,64 +32,24 @@ app.post('/delete_process', (request, response) => {
   topic.delete_process(request, response);
 })
 
+app.get('/author', (request, response) => {
+  author.home(request, response);
+})
+
+app.post('/author/create_process', (request, response) => {
+  author.create_process(request, response);
+})
+
+app.get('/author/update/:pageId', (request, response) => {
+  author.update(request, response);
+})
+
+app.post('/author/update_process', (request, response) => {
+  author.update_process(request, response);
+})
+
+app.post('/author/delete_process', (request, response) => {
+  author.delete_process(request, response);
+})
+
 app.listen(3000, () => console.log("Example app listening on port 3000!"))
-
-/*
-var http = require('http');
-var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var db = require('./lib/db.js');
-var topic = require('./lib/topic.js');
-var author = require('./lib/author.js');
-
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-
-    if(pathname === '/') {
-      if(queryData.id === undefined) {
-        topic.home(request, response);
-      }
-      else {
-        topic.page(request, response);
-      }
-    }
-    else if(pathname === '/create') {
-      topic.create(request, response);
-    }
-    else if(pathname === '/create_process') {
-      topic.create_process(request, response);
-    }
-    else if(pathname === '/update') {
-      topic.update(request, response);
-    }
-    else if(pathname === '/update_process') {
-      topic.update_process(request, response);
-    }
-    else if(pathname === '/delete_process') {
-      topic.delete_process(request, response);
-    }
-    else if(pathname === '/author') {
-      author.home(request, response);
-    }
-    else if(pathname === '/author/create_process') {
-      author.create_process(request, response);
-    }
-    else if(pathname === '/author/update') {
-      author.update(request, response);
-    }
-    else if(pathname === '/author/update_process') {
-      author.update_process(request, response);
-    }
-    else if(pathname === '/author/delete_process') {
-      author.delete_process(request, response);
-    }
-    else {
-      response.writeHead(404);
-      response.end('Not found');
-    }
-});
-app.listen(3000);
-*/
