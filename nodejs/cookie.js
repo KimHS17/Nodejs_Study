0d3ee9803613bf00fb0d4f566740a03a164bf1d9
KimHS17 <skes01149@naver.com>
2023-08-27 15:47:05 +0900

    Cookie_8

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -13,7 +13,9 @@ http.createServer(function(request, response) {
             'tasty_cookie=strawberry',
             `Permanent=cookies; Max-Age=${60*60*24*30}`, //쿠키가 삭제될 시간 지정
             'Secure=Secure; Secure', //https를 쓰는 경우에만 전송
-            'HttpOnly=HttpOnly; HttpOnly' //http로 접속할 경우에만 접근 가능
+            'HttpOnly=HttpOnly; HttpOnly', //http로 접속할 경우에만 접근 가능
+            'Path=Path; Path=/cookie', //특정 경로(하위 폴더 포함)에서만 쿠키 확인 가능
+            'Domain=Domain; Domain=test.com' //특정 도메인에서만 쿠키 확인 가능
             ]
     });
     response.end('Cookie!!');
