9aee8dea22ae848ff3509c637d6c3b3aa17b8c7f
KimHS17 <skes01149@naver.com>
2023-08-27 15:36:55 +0900

    Cookie_7

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -11,7 +11,9 @@ http.createServer(function(request, response) {
         'Set-Cookie':[
             'yummy_cookie=choco',
             'tasty_cookie=strawberry',
-            `Permanent=cookies; Max-Age=${60*60*24*30}`
+            `Permanent=cookies; Max-Age=${60*60*24*30}`, //쿠키가 삭제될 시간 지정
+            'Secure=Secure; Secure', //https를 쓰는 경우에만 전송
+            'HttpOnly=HttpOnly; HttpOnly' //http로 접속할 경우에만 접근 가능
             ]
     });
     response.end('Cookie!!');
