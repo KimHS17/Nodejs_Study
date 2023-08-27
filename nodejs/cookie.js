92f79d5c8755e573a2e9a89fab7193652a442c79
KimHS17 <skes01149@naver.com>
2023-08-27 15:15:28 +0900

    Cookie_4

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,5 +1,12 @@
 var http = require('http');
+var cookie = require('cookie');
+
 http.createServer(function(request, response) {
+    var cookies = {};
+    if(request.headers.connection !== undefined) {
+        cookies = cookie.parse(request.headers.cookie);
+    }
+    console.log(cookies);
     response.writeHead(200, {
         'Set-Cookie':['yummy_cookie=choco', 'tasty_cookie=strawberry']
     });
